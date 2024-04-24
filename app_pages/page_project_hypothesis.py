c2d39df72ccfbc98d8bc9f38f4b8e2d406023b77
sibekov <vusimuzij.sibeko@gmail.com>
2024-04-24 19:22:14 +0000

    Add a page showing the hypothesis

---
+++ app_pages/page_project_hypothesis.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def page_project_hypothesis_body():
+    st.write("### Project Hypothesis and Validation")
+
+    st.success(
+        f"* We suspect powdery mildew leaves have clear marks/signs, "
+        f"typically the white powder can be seen and can be any where on the leaf.. \n\n"
+        f"* An Image Montage shows that typically a powdery mildew leafs white marks over it. "
+        f"Average Image, Variability Image and Difference between Averages studies did not reveal "
+        f"a clear pattern to differentiate one from another.")
